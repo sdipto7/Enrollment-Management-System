3ff612fb10062a2618458dd590fd438e8afcd3f4
sdipto7 <rumi.dipto@therapservices.net>
2021-08-29 15:17:58 +0600

    Fixed some issues of Scanner

--- src/main/java/net/therap/hibernet/controller/CourseController.java
+++ src/main/java/net/therap/hibernet/controller/CourseController.java
@@ -15,16 +15,16 @@ public class CourseController {
 
     private CourseService courseService;
 
-    public CourseController(){
+    public CourseController() {
         courseService = new CourseService();
     }
 
-    public void viewAllCourses(){
+    public void viewAllCourses() {
         List<Course> courseList = courseService.getCourseList();
         EnrollmentView.printCourseList(courseList);
     }
 
-    public void addCourse(){
+    public void addCourse() {
         Scanner input = new Scanner(System.in);
 
         System.out.println("Enter new course code: ");
@@ -34,9 +34,11 @@ public class CourseController {
         String courseTitle = input.nextLine();
 
         courseService.addCourse(courseCode, courseTitle);
+
+        input.close();
     }
 
-    public void updateCourse(){
+    public void updateCourse() {
         Scanner input = new Scanner(System.in);
 
         System.out.println("Enter the id of the course: ");
@@ -44,31 +46,33 @@ public class CourseController {
 
         System.out.println("Enter the new course code: ");
         String newCourseCode = input.nextLine();
-        input.nextLine();
 
         System.out.println("Enter the new course title: ");
         String newCourseTitle = input.nextLine();
 
         courseService.updateCourse(courseId, newCourseCode, newCourseTitle);
+        input.close();
     }
 
-    public void viewCourse(){
+    public void viewCourse() {
         Scanner input = new Scanner(System.in);
 
         System.out.println("Enter the course code: ");
-        Long courseId = input.nextLong();
+        long courseId = input.nextLong();
 
         Course course = courseService.getCourse(courseId);
 
         EnrollmentView.printCourse(course);
+        input.close();
     }
 
-    public void deleteCourse(){
+    public void deleteCourse() {
         Scanner input = new Scanner(System.in);
 
         System.out.println("Enter the course code: ");
         long courseId = input.nextLong();
 
         courseService.deleteCourse(courseId);
+        input.close();
     }
 }
