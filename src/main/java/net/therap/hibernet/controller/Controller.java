a37d2c6d5015cac6a1dbc799d7acd9c33aea868e
sdipto7 <rumi.dipto@therapservices.net>
2021-08-29 16:09:39 +0600

    Removed unused imports from Controller class

--- src/main/java/net/therap/hibernet/controller/Controller.java
+++ src/main/java/net/therap/hibernet/controller/Controller.java
@@ -1,13 +1,5 @@
 package net.therap.hibernet.controller;
 
-import net.therap.hibernet.View.EnrollmentView;
-import net.therap.hibernet.domain.Course;
-import net.therap.hibernet.domain.Enrollment;
-import net.therap.hibernet.domain.User;
-import net.therap.hibernet.service.EnrollmentService;
-import net.therap.hibernet.service.UserService;
-
-import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -23,19 +15,19 @@ public class Controller {
         while (true) {
             System.out.println("Press 0 to exit");
             System.out.println("Press 1 to view courses");
-            System.out.println("Press 2 to view users"); //u
+            System.out.println("Press 2 to view users");
             System.out.println("Press 3 to view course enrollment details");
             System.out.println("Press 4 to add a new course");
-            System.out.println("Press 5 to add a new user"); //u
+            System.out.println("Press 5 to add a new user");
             System.out.println("Press 6 to add a new enrollment");
             System.out.println("Press 7 to update a course information");
-            System.out.println("Press 8 to update an user information"); //u
+            System.out.println("Press 8 to update an user information");
             System.out.println("Press 9 to update an enrollment information");
             System.out.println("Press 10 to view a specific course information");
-            System.out.println("Press 11 to view a specific user information"); //u
+            System.out.println("Press 11 to view a specific user information");
             System.out.println("Press 12 to view a specific enrollment information");
             System.out.println("Press 13 to delete a course information");
-            System.out.println("Press 14 to delete an user information"); //u
+            System.out.println("Press 14 to delete an user information");
             System.out.println("Press 15 to delete an enrollment information");
 
             int operation = input.nextInt();
@@ -49,19 +41,11 @@ public class Controller {
     }
 
     public static void executeOperation(int operation) {
-        EnrollmentService enrollmentService = new EnrollmentService();
+        EnrollmentController enrollmentController = new EnrollmentController();
 
         CourseController courseController = new CourseController();
 
         UserController userController = new UserController();
-
-        long courseId;
-
-        long userId;
-
-        long enrollmentId;
-
-        Scanner input = new Scanner(System.in);
 
         switch (operation) {
             case 1:
@@ -73,8 +57,7 @@ public class Controller {
                 break;
 
             case 3:
-                List<Enrollment> enrollmentList = enrollmentService.getEnrollmentList();
-                EnrollmentView.printEnrollmentList(enrollmentList);
+                enrollmentController.viewAllEnrollments();
                 break;
 
             case 4:
@@ -86,15 +69,7 @@ public class Controller {
                 break;
 
             case 6:
-                System.out.println("Enter user id");
-                userId = input.nextLong();
-                input.nextLine();
-
-                System.out.println("Enter course code");
-                courseId = input.nextLong();
-                input.nextLine();
-
-                enrollmentService.addEnrollment(userId, courseId);
+                enrollmentController.addEnrollment();
                 break;
 
             case 7:
@@ -106,16 +81,7 @@ public class Controller {
                 break;
 
             case 9:
-                System.out.println("Enter the id of enrollment: ");
-                enrollmentId = input.nextLong();
-
-                System.out.println("Enter the new user id: ");
-                long newUserId = input.nextLong();
-
-                System.out.println("Enter the new course id: ");
-                long newCourseId = input.nextLong();
-
-                enrollmentService.updateEnrollment(enrollmentId, newUserId, newUserId);
+                enrollmentController.updateEnrollment();
                 break;
 
             case 10:
@@ -127,12 +93,7 @@ public class Controller {
                 break;
 
             case 12:
-                System.out.println("Enter the enrollment id: ");
-                enrollmentId = input.nextLong();
-
-                Enrollment enrollment = enrollmentService.getEnrollment(enrollmentId);
-
-                EnrollmentView.printEnrollment(enrollment);
+                enrollmentController.viewEnrollment();
                 break;
 
             case 13:
@@ -144,10 +105,7 @@ public class Controller {
                 break;
 
             case 15:
-                System.out.println("Enter the enrollment id: ");
-                enrollmentId = input.nextLong();
-
-                enrollmentService.deleteEnrollment(enrollmentId);
+                enrollmentController.deleteEnrollment();
                 break;
 
             default:
