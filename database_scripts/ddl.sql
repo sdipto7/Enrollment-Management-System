5ffe26434fb70f6a9a92958ea1bfde142a148839
sdipto7 <rumi.dipto@therapservices.net>
2021-08-31 16:55:33 +0600

    Added the ddl scripts

---
+++ database_scripts/ddl.sql
@@ -0,0 +1,19 @@
+CREATE TABLE course(
+id INT NOT NULL AUTO_INCREMENT,
+course_title VARCHAR(100)
+course_code VARCHAR(7),
+PRIMARY KEY("id")
+);
+
+CREATE TABLE user(
+id INT NOT NULL,
+name varchar(100),
+PRIMARY KEY("id")
+);
+
+CREATE TABLE enrollment(
+id INT NOT NULL AUTO_INCREMENT,
+user_id INT NOT NULL,
+course_id INT NOT NULL,
+PRIMARY KEY("id")
+);
