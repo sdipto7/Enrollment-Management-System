301f3f192fa9f6f14c8c52664c5a238dbf737e39
sdipto7 <rumi.dipto@therapservices.net>
2021-08-31 15:02:06 +0600

    Updated the generation strategy to IDENTITY in the primary key of Course and Enrollment entity

--- src/main/java/net/therap/hibernet/domain/Enrollment.java
+++ src/main/java/net/therap/hibernet/domain/Enrollment.java
@@ -15,7 +15,7 @@ public class Enrollment implements Comparable<Enrollment>, Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     @ManyToOne
